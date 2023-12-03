1f8fbff02e34a26de9915eb7b0bb52a71f0341c5
tomcru <hello@tomrumpf.com>
2023-12-03 16:43:42 -0500

    test: add format, add, general tests, handle all tests in UTC timezone

---
+++ src/__tests__/add.test.ts
@@ -0,0 +1,58 @@
+import { describe, expect, it } from 'vitest';
+import HolyTime from '..';
+
+describe('general', () => {
+  it('should handle adding zero', () => {
+    const date = new HolyTime(new Date('2023-01-01T00:00:00.000Z'));
+    expect(date.add(0, 'milliseconds').getTime()).toBe(date.getTime());
+  });
+
+  it('should handle negative adding', () => {
+    const date = new HolyTime(new Date('2023-01-01T01:00:00.000Z'));
+    const expected = new Date('2023-01-01T00:00:00.000Z');
+    expect(date.add(-60, 'minutes').getDate()).toEqual(expected);
+  });
+
+  it('should handle adding across day boundary', () => {
+    const date = new HolyTime(new Date('2023-01-01T23:00:00.000Z'));
+    const expected = new Date('2023-01-02T01:00:00.000Z');
+    expect(date.add(2, 'hours').getDate()).toEqual(expected);
+  });
+
+  it('should handle adding across month boundary', () => {
+    const date = new HolyTime(new Date('2023-01-31T23:00:00.000Z'));
+    const expected = new Date('2023-02-01T01:00:00.000Z');
+    expect(date.add(2, 'hours').getDate()).toEqual(expected);
+  });
+
+  it('should handle adding across year boundary', () => {
+    const date = new HolyTime(new Date('2023-12-31T23:00:00.000Z'));
+    const expected = new Date('2024-01-01T01:00:00.000Z');
+    expect(date.add(2, 'hours').getDate()).toEqual(expected);
+  });
+
+  it('should handle adding across leap year (2024) boundary', () => {
+    const date = new HolyTime(new Date('2023-12-31T23:00:00.000Z'));
+    const expected = new Date('2024-01-01T01:00:00.000Z');
+    expect(date.add(2, 'hours').getDate()).toEqual(expected);
+  });
+
+  it('should handle adding in leap year (2024)', () => {
+    const date = new HolyTime(new Date('2024-02-28T00:00:00.000Z'));
+    const expected = new Date('2024-02-29T00:00:00.000Z');
+    expect(date.add(1, 'days').getDate()).toEqual(expected);
+  });
+
+  it('should handle large adding values', () => {
+    const date = new HolyTime(new Date('2023-01-01T00:00:00.000Z'));
+    const yearInMs = 1000 * 60 * 60 * 24 * 365;
+    const expected = new Date('2032-12-29T00:00:00.000Z'); // 2 leap years == -2 days
+    expect(date.add(10 * yearInMs, 'milliseconds').getDate()).toEqual(expected);
+  });
+
+  it('should handle adding with non-standard units', () => {
+    const date = new HolyTime(new Date('2023-01-01T00:00:00.000Z'));
+    // @ts-expect-error
+    expect(() => date.add(1, 'nonStandardUnit')).toThrow('Invalid unit: nonStandardUnit');
+  });
+});
--- src/__tests__/general.test.ts
+++ src/__tests__/general.test.ts
@@ -25,4 +25,51 @@ describe('general', () => {
 
     expect(holyTime.getISOString()).toBe(date.toISOString());
   });
+
+  it('should return date', () => {
+    const date = new Date('2023-01-01T00:02:00.000Z');
+
+    const holyTime = new HolyTime(date);
+
+    expect(holyTime.getDate()).toBe(date);
+  });
+
+  it('should subtract time correctly', () => {
+    const date = new Date('2023-01-01T01:00:00.000Z');
+    const holyTime = new HolyTime(date);
+    const expected = new Date('2023-01-01T00:00:00.000Z');
+
+    expect(holyTime.subtract(1, 'hours').getDate()).toEqual(expected);
+  });
+
+  it('should check if one date is after another correctly', () => {
+    const earlierDate = new HolyTime(new Date('2023-01-01T00:00:00.000Z'));
+    const laterDate = new HolyTime(new Date('2023-01-02T00:00:00.000Z'));
+
+    expect(earlierDate.isAfter(laterDate)).toBe(false);
+    expect(laterDate.isAfter(earlierDate)).toBe(true);
+  });
+
+  it('should check if one date is before another correctly', () => {
+    const earlierDate = new HolyTime(new Date('2023-01-01T00:00:00.000Z'));
+    const laterDate = new HolyTime(new Date('2023-01-02T00:00:00.000Z'));
+
+    expect(earlierDate.isBefore(laterDate)).toBe(true);
+    expect(laterDate.isBefore(earlierDate)).toBe(false);
+  });
+
+  it('should calculate time difference', () => {
+    const start = new HolyTime(new Date('2023-01-01T00:00:00.000Z'));
+    const end = new HolyTime(new Date('2023-01-01T01:00:00.000Z'));
+
+    expect(HolyTime.between(start, end)).toBe(3_600_000); // 1 hour in milliseconds
+  });
+
+  it('should check if date is weekend', () => {
+    const weekday = new HolyTime(new Date('2023-01-04T01:00:00.000Z'));
+    const weekend = new HolyTime(new Date('2023-01-07T00:00:00.000Z'));
+
+    expect(weekday.isWeekend()).toBe(false);
+    expect(weekend.isWeekend()).toBe(true);
+  });
 });
